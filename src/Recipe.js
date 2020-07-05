e7d0c0fd84393ba9650b636fcbb4eb8f40b42ef7
UlrichDinh168 <duong0028@gmail.com>
2020-07-05 18:55:29 +0300

    add hover effect

--- src/Recipe.js
+++ src/Recipe.js
@@ -3,15 +3,17 @@ import styles from "./Recipe.module.css";
 
 const Recipe = ({ title, calories, image, ingredients }) => {
   return (
-    <div className={styles.recipe}>
-      <h1>{title}</h1>
-      <ul>
-        {ingredients.map(ingredient => (
-          <li>{ingredient.text}</li>
-        ))}
-      </ul>
-      <p className={styles.calories}>Calories : {Math.floor(calories)}</p>
-      <img className={styles.image} src={image} alt="" />
+    <div className="container">
+      <div className={styles.recipe}>
+        <h1>{title}</h1>
+        <ul>
+          {ingredients.map((ingredient) => (
+            <li>{ingredient.text}</li>
+          ))}
+        </ul>
+        <p className={styles.calories}>Calories : {Math.floor(calories)}</p>
+        <img className={styles.image} src={image} alt="" />
+      </div>
     </div>
   );
 };
